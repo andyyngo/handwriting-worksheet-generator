439580107b4669e3b6f065bb1908bb0cda6bd95c
ngothanhnghi@gmail.com <ngothanhnghi@gmail.com>
2025-08-10 16:49:17 -0500

    Add vite cloudflare plugin for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from "@cloudflare/vite-plugin"
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss(), cloudflare()
   ],
 })
